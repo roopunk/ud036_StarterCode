fc06f6e7a0b1492cbcc7a2bafb8129d17b2e65ef
Roopam Poddar <mailroopam@gmail.com>
2018-03-05 08:35:25 +0530

    adding more attributes for the movie

---
+++ entertainment_center.py
@@ -0,0 +1,17 @@
+import media
+
+into_the_wild = media.Movie("Into The Wild",
+                            "When a man leaves his possessions to explore the world as a hitchhiker but gets trapped in the wild"
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/8/8a/Into-the-wild.jpg/220px-Into-the-wild.jpg",
+                            "https://www.youtube.com/watch?v=2LAuzT_x8Ek",
+                            "2007",
+                            "Emile Hirsch, Vince Vaughn, Catherine Keener")
+
+the_dark_knight = None
+black_panther = None
+dunkirk = None
+the_bourne_identity = None
+wonder_woman = None
+interstellar = None
+inglorious_bastards = None
+django_unchained = None
